02ed10099e527e69f00bb812d6aff00ad459e656
AndreiZiureaev <48158223+AndreiZiureaev@users.noreply.github.com>
2019-04-08 19:57:42 +0100

    Update obstacle.js

--- obstacle.js
+++ obstacle.js
@@ -3,21 +3,18 @@ class obstacle{
       static function move
   */
   constructor(hole){
+    this.margin = height/10;
     this.x=width;
+    this.width = 50;
     this.hole=hole;
     this.y=0;
-    this.holeStart=Math.floor((Math.random() * (height-height/5)) + height/5);
+    this.holeStart=Math.floor(Math.random() * (height-hole-this.margin*2) + this.margin);
   }
-
-
 
   display(){
     fill(126);
-    rect(this.x,this.y,20,this.holeStart);
-    rect(this.x,this.holeStart+this.hole,20,height);
-    this.x--;
+    rect(this.x,this.y,this.width,this.holeStart);
+    rect(this.x,this.holeStart+this.hole,this.width,height);
+    this.x -= 5;
   }
-  
-
-
 }
