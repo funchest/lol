1b7b1d76cd676bfdd5dd48ebc90fe78db04a02fe
AndreiZiureaev <48158223+AndreiZiureaev@users.noreply.github.com>
2019-04-08 19:58:19 +0100

    Update scetch.js

--- scetch.js
+++ scetch.js
@@ -1,36 +1,56 @@
-var obstacles=[];
-var hole = 50;
-var maxObstacles=5;
-function setup(){
-  createCanvas(800,600);
-}
-function draw(){
-  background(0);
-  shallIaddobstaces();
-  for(var i =0 ; i<obstacles.length;i++){
-    obstacles[i].display();
-  }
-  
+let width = 640;
+let height = 480;
+let bird = new Bird(width, height);
+let endGame = false;
 
+var obstacles=[];
+var hole = 200;
+var maxObstacles=5;
+
+function setup(){
+    createCanvas(width, height);
 }
-  function go(){
-    if(obstacles.length==0){
-      obstacles.push(new obstacle(hole));
+
+function draw(){
+    if (bird.checkColl(obstacles)) {
+        endGame = true;
+        background(200, 20, 20);
+        noLoop();
+        return;
     }
 
-  }
+    background(0);
+    shallIaddobstaces();
+    for(var i =0 ; i<obstacles.length;i++){
+        obstacles[i].display();
+    }
 
+    bird.display();
+}
+
+function keyPressed() {
+    if (keyCode === ENTER) {
+        bird.jump();
+    } else if (keyCode === BACKSPACE) {
+        if (endGame) {
+            endGame = false;
+            bird = new Bird(width, height);
+            obstacles = [];
+            loop();
+        }
+    }
+}
 
 function shallIaddobstaces(){
-  if(obstacles.length==0){
-    obstacles.push(new obstacle(hole));
-  }
-  else if(obstacles.length>0&&obstacles.length<maxObstacles){
-    if(obstacles[obstacles.length-1].x<width-160){
-      obstacles.push(new obstacle(hole));
+    if(obstacles.length==0){
+        obstacles.push(new obstacle(hole));
     }
-  }
-  if(obstacles[0].x<0){
-    obstacles.shift();
-  }
+    else if(obstacles.length>0&&obstacles.length<maxObstacles){
+        if(obstacles[obstacles.length-1].x<width-160){
+            obstacles.push(new obstacle(hole));
+        }
+    }
+    if(obstacles[0].x<0){
+        obstacles.shift();
+    }
 }
