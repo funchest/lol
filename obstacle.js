9315b5c4ab46df76fddd63964d853cf4855c1feb
funchest <45046290+funchest@users.noreply.github.com>
2019-04-08 17:56:31 +0100

    Add files via upload

---
+++ obstacle.js
@@ -0,0 +1,13 @@
+class obstacle{
+  /*   x y
+      static function move
+  */
+  constructor(a,b){
+    rect(20,20,20,20);
+  }
+  display(){
+    rect(20,20,20,20);
+  }
+
+
+}
---
+++ scetch.js
@@ -0,0 +1,7 @@
+var a=0;
+function setup(){
+  createCanvas(800,600);
+}
+function draw(){
+  var a = new obstacle(50,50);
+}
