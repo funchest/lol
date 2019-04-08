5dbbdd11661458f71f77aeaa87b2cf15b4c7d25c
funchest <45046290+funchest@users.noreply.github.com>
2019-04-08 19:00:23 +0100

    Add files via upload

--- obstacle.js
+++ obstacle.js
@@ -2,12 +2,22 @@ class obstacle{
   /*   x y
       static function move
   */
-  constructor(a,b){
-    rect(20,20,20,20);
+  constructor(hole){
+    this.x=width;
+    this.hole=hole;
+    this.y=0;
+    this.holeStart=Math.floor((Math.random() * (height-height/5)) + height/5);
   }
+
+
+
   display(){
-    rect(20,20,20,20);
+    fill(126);
+    rect(this.x,this.y,20,this.holeStart);
+    rect(this.x,this.holeStart+this.hole,20,height);
+    this.x--;
   }
+  
 
 
 }
--- scetch.js
+++ scetch.js
@@ -1,7 +1,36 @@
-var a=0;
+var obstacles=[];
+var hole = 50;
+var maxObstacles=5;
 function setup(){
   createCanvas(800,600);
 }
 function draw(){
-  var a = new obstacle(50,50);
+  background(0);
+  shallIaddobstaces();
+  for(var i =0 ; i<obstacles.length;i++){
+    obstacles[i].display();
+  }
+  
+
+}
+  function go(){
+    if(obstacles.length==0){
+      obstacles.push(new obstacle(hole));
+    }
+
+  }
+
+
+function shallIaddobstaces(){
+  if(obstacles.length==0){
+    obstacles.push(new obstacle(hole));
+  }
+  else if(obstacles.length>0&&obstacles.length<maxObstacles){
+    if(obstacles[obstacles.length-1].x<width-160){
+      obstacles.push(new obstacle(hole));
+    }
+  }
+  if(obstacles[0].x<0){
+    obstacles.shift();
+  }
 }
